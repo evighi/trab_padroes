5816f245fede78f4f598493811a5ab9ed3f3acb8
evighi <eduardovighi8@gmail.com>
2024-12-02 21:12:17 -0300

    Update semPadrao.ts

--- factory_method/semPadrao.ts
+++ factory_method/semPadrao.ts
@@ -1,8 +1,19 @@
-class Car {
-    constructor(public model: string, public engine: string) {}
+class Vehicle {
+  constructor(public name: string, public type: string) {}
+}
+
+class VehicleFactory {
+  createVehicle(name: string, type: string): Vehicle {
+    if (type === 'car') {
+      return new Vehicle(name, 'car');
+    } else if (type === 'motorcycle') {
+      return new Vehicle(name, 'motorcycle');
+    } else {
+      throw new Error('Unknown vehicle type');
+    }
   }
-  
-  const car = new Car("Sedan", "V6");
-  console.log("Without Factory Method:", car); 
-  
-  
+}
+
+const factory = new VehicleFactory();
+const car = factory.createVehicle('Car A', 'car');
+const motorcycle = factory.createVehicle('Motorcycle B', 'motorcycle');
